4751b3756a90960f4a9a68f00f2b30693ee283db
Capexoto <gustavo3x2@gmail.com>
2021-07-20 23:47:09 -0300

    CR#02

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const ejs = require('ejs')
 
 const app = express()
 
-app.get('/', (req, res) =>{res.send('Hello')})
+app.use(bodyParser.urlencoded({extended : true}))
+var publicDir = require('path').join(__dirname,'/public')
+app.use(express.static(publicDir));
+app.use('/', express.static(__dirname + '/www')); // redirect root
+app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js'));//redirect bootstrap
+app.use('/js', express.static(__dirname + '/node_modules/jquery/dist'));// redirect JS Jquery
+app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'));//redirect CSS bootstrap
+app.use('/style', express.static(__dirname + '/style/'));//redirect CSS bootstrap
+
+
+app.set('view engine','ejs')
+app.get('/', (req, res) =>{res.render('index')})
 
 app.listen(3000)
